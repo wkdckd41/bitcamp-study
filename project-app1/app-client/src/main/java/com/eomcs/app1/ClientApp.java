b5f98d5a84f9f1ab2ac633ea79e1ee9bcf481474
wkdckd41 <wkdckd41@gmail.com>
2022-01-27 17:21:33 +0900

    ..

--- project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
@@ -10,7 +10,7 @@ public class ClientApp {
     System.out.println("[계산기 클라이언트]");
 
     // 서버 애플리케이션과 네트워크 연결을 수행한다.
-    Socket socket = new Socket("192.168.0.4", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
+    Socket socket = new Socket("192.168.0.191", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
     System.out.println("서버와 연결되었음!");
 
     // 데이터를 주고 받기 위한 입출력 스트림을 준비한다.
@@ -18,7 +18,7 @@ public class ClientApp {
     Scanner in = new Scanner(socket.getInputStream());
 
     // 서버에 데이터를 보낸다.
-    out.println("강사");
+    out.println("JangChang");
 
     // 서버가 응답한 데이터를 읽는다.
     String response = in.nextLine(); // 서버가 한 줄의 문자열을 보낼 때까지 리턴하지 않는다.
