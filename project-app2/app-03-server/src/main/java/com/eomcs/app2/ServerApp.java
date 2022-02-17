eca38e83d706bfeac9e73cd536e5a56c52602dfb
wkdckd41 <wkdckd41@gmail.com>
2022-02-17 23:37:05 +0900

    ..

---
+++ project-app2/app-03-server/src/main/java/com/eomcs/app2/ServerApp.java
@@ -0,0 +1,111 @@
+package com.eomcs.app2;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import com.eomcs.app2.table.ScoreTable;
+import com.eomcs.app2.vo.Score;
+
+public class ServerApp {
+
+  public static void main(String[] args) {
+    new ServerApp().service();
+  }
+
+  public void service() {
+    try (ServerSocket serverSocket = new ServerSocket(3336);) {
+      System.out.println("서버 실행 중...");
+
+      while (true) {
+        new RequestHandler(serverSocket.accept()).start();
+      } // while (true)
+    } catch (Exception e) {
+      System.out.println("서버 실행 오류!");
+    }
+
+    System.out.println("종료!");
+  }
+
+  private static class RequestHandler extends Thread {
+
+    Socket socket;
+
+    public RequestHandler(Socket socket) {
+      this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+      try (Socket socket2 = socket;
+          ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+          ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());) {
+
+
+        System.out.println("클라이언트가 접속했습니다.");
+
+        while (true) {
+          String command = in.readUTF();
+          if (command.equals("quit")) {
+            break;
+          }
+          try {
+            switch (command) {
+              case "insert":
+                Score score = (Score) in.readObject();
+                int count = ScoreTable.insert(score);
+                out.writeUTF("success");
+                out.writeInt(count);
+                break;
+              case "selectList":
+                Score[] scores = ScoreTable.selectList();
+                out.writeUTF("success");
+                out.writeObject(scores);
+                break;
+              case "selectOne":
+                int no = in.readInt();
+                score = ScoreTable.selectOne(no);
+                out.writeUTF("success");
+                out.writeObject(score);
+                break;
+              case "update":
+                no = in.readInt();
+                score = (Score) in.readObject();
+                count = ScoreTable.update(no, score);
+                out.writeUTF("success");
+                out.writeInt(count);
+                break;
+              case "delete":
+                no = in.readInt();
+                count = ScoreTable.delete(no);
+                out.writeUTF("success");
+                out.writeInt(count);
+                break;
+              default:
+                out.writeUTF("fail");
+                out.writeUTF("해당 명령을 지원하지 않습니다.");
+            }
+            out.flush();
+          } catch (Exception e) {
+            out.writeUTF("fail");
+            out.writeUTF("실행 오류: " + e.getMessage());
+            out.flush();
+          }
+        } // while (true)
+        System.out.println("클라이언트와의 연결을 끊었습니다.");
+
+      } catch (Exception e) {
+        System.out.println("클라이언트와 통신 중 오류 발생!");
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ project-app2/app-04-client/src/main/java/com/eomcs/app2/handler/ScoreHandler.java
@@ -0,0 +1,84 @@
+package com.eomcs.app2.handler;
+
+import com.eomcs.app2.net.ScoreTableProxy;
+import com.eomcs.app2.vo.Score;
+import com.eomcs.util.Prompt;
+
+public class ScoreHandler {
+
+  ScoreTableProxy scoreTable;
+
+  public ScoreHandler(ScoreTableProxy scoreTable) {
+    this.scoreTable = scoreTable;
+  }
+
+  public void create() throws Exception {
+    Score score = new Score();
+    score.setName(Prompt.promptString("이름? "));
+    score.setKor(Prompt.promptInt("국어? "));
+    score.setEng(Prompt.promptInt("영어? "));
+    score.setMath(Prompt.promptInt("수학? "));
+
+    int count = scoreTable.insert(score);
+    if (count == 1) {
+      System.out.println("입력했습니다.");
+    } else {
+      System.out.println("입력하지 못했습니다.");
+    }
+  }
+
+  public void list() throws Exception {
+    Score[] scores = scoreTable.selectList();
+    int count = 0;
+    for (Score score : scores) {
+      System.out.printf("%d: %s, %d, %.1f\n",
+          count++,
+          score.getName(), 
+          score.getSum(),
+          score.getAverage());
+    }
+  }
+
+  public void detail() throws Exception {
+    int no = Prompt.promptInt("번호? ");
+
+    Score score = scoreTable.selectOne(no);
+
+    System.out.printf("이름: %s\n", score.getName());
+    System.out.printf("국어: %d\n", score.getKor());
+    System.out.printf("영어: %d\n", score.getEng());
+    System.out.printf("수학: %d\n", score.getMath());
+    System.out.printf("합계: %d\n", score.getSum());
+    System.out.printf("평균: %.1f\n", score.getAverage());
+  }
+
+  public void update() throws Exception {
+    int no = Prompt.promptInt("번호? ");
+
+    Score old = scoreTable.selectOne(no);
+
+    Score score = new Score();
+    score.setName(Prompt.promptString("이름(%s)? ", old.getName()));
+    score.setKor(Prompt.promptInt("국어(%d)? ", old.getKor()));
+    score.setEng(Prompt.promptInt("영어(%d)? ", old.getEng()));
+    score.setMath(Prompt.promptInt("수학(%d)? ", old.getMath()));
+
+    int count = scoreTable.update(no, score);
+    if (count == 1) {
+      System.out.println("변경했습니다.");
+    } else {
+      System.out.println("변경하지 못했습니다.");
+    }
+  }
+
+  public void delete() throws Exception {
+    int no = Prompt.promptInt("번호? ");
+
+    int count = scoreTable.delete(no);
+    if (count == 1) {
+      System.out.println("삭제했습니다.");
+    } else {
+      System.out.println("삭제하지 못했습니다.");
+    }
+  }
+}
